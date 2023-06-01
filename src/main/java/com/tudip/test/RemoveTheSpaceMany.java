7a10e49678b7a4dbf62f9e41bd6272eecc79b1f1
anderanjith@gmail.com <anderanjith@gmail.com>
2023-06-01 22:03:46 +0530

    commit from IJ

--- src/main/java/com/tudip/test/RemoveTheSpaceMany.java
+++ src/main/java/com/tudip/test/RemoveTheSpaceMany.java
@@ -4,10 +4,16 @@ public class RemoveTheSpaceMany {
 
 	public static void main(String[] args) {
 
-		String str = "Write a program         to remove all the     extra spaces         from a        paragraph/string     ";
+		String str = "I      am      a      james      bond     .";
+		
+		
+
+//		String str = "Write a program         to remove all the     extra spaces         from a        paragraph/string     ";
 
 		String replaceAll = str.replaceAll("\\s+", " ");
+//		String replaceAll2 = replaceAll.replace(, 0)
 
-		System.out.println(replaceAll);
+
+//		System.out.println(replaceAll2);
 	}
 }
